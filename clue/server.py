ca8f3bf2a5917bfae387d306051fe2bd856ade1c
Shafique Arnab <shafiquearnab@gmail.com>
2017-10-09 16:31:24 -0400

    Added URI support

--- clue/server.py
+++ clue/server.py
@@ -2,8 +2,9 @@ import sys
 from twisted.internet import reactor
 from twisted.python import log
 from clue.websocket.factory import ClueServerFactory
+from autobahn.websocket.util import parse_url
 
-def launch_server(port):
+def launch_server(uri):
 	# Start logging to STDOUT
 	log.startLogging(sys.stdout)
 
@@ -11,7 +12,10 @@ def launch_server(port):
     # The factory is responsible for creating the protocol
     # for each connection. It is the shared data-model
     # across all connections.
-	factory = ClueServerFactory()
+	factory = ClueServerFactory(uri)
+
+	# Parse the uri
+	isSecure, host, port, resource, path, params = parse_url(uri)
 
 	# Start the Twisted reactor
 	# Main thread is now blocked
